b47fe05748876423daafbfaff7c87f40cffe3cc7
NotSoupCarry <giuseppeforce36@gmail.com>
2025-12-13 00:34:39 +0100

    addings

--- swww/wallpaperManager.sh
+++ swww/wallpaperManager.sh
@@ -26,8 +26,6 @@ SELECTED_FILE="$WALLPAPER_DIR/$SELECTED_FILENAME"
     --transition-duration 5 \
     --transition-fps 60
 
-pkill swaync 
-swaync
-wal -i "$SELECTED_FILE"
+pkill swaync & swaync & wal -i "$SELECTED_FILE"
 
 # --- End of Script ---
