fb3021061de5a49426f05c04ecb31bab5c3f0840
NotSoupCarry <giuseppeforce36@gmail.com>
2025-12-22 17:55:39 +0100

    addings

--- swww/wallpaperManager.sh
+++ swww/wallpaperManager.sh
@@ -9,34 +9,34 @@ cd "$WALLPAPER_DIR"
 # Crea lista con icone per rofi
 WALLPAPER_LIST=""
 for img in *.jpg *.jpeg *.png; do
-    [ -f "$img" ] || continue
-    WALLPAPER_LIST+="$img\0icon\x1f$WALLPAPER_DIR/$img\n"
+  [ -f "$img" ] || continue
+  WALLPAPER_LIST+="$img\0icon\x1f$WALLPAPER_DIR/$img\n"
 done
 
 # Mostra rofi con anteprime centrate e uniformi
 SELECTED_FILENAME=$(echo -en "$WALLPAPER_LIST" | rofi -dmenu \
-                    -i \
-                    -p "Select a wallpaper" \
-                    -config "$ROFI_THEME_FILE" \
-                    -show-icons \
-                    -theme-str 'element { orientation: vertical; padding: 10px; }' \
-                    -theme-str 'element-icon { size: 25%; horizontal-align: 0.5; vertical-align: 0.5; }' \
-                    -theme-str 'element-text { enabled: false; }' \
-                    -theme-str 'listview { columns: 3; lines: 3; spacing: 3px; fixed-columns: true; flow: horizontal; }')
+  -i \
+  -p "Select a wallpaper" \
+  -config "$ROFI_THEME_FILE" \
+  -show-icons \
+  -theme-str 'element { orientation: vertical; padding: 10px; }' \
+  -theme-str 'element-icon { size: 25%; horizontal-align: 0.5; vertical-align: 0.5; }' \
+  -theme-str 'element-text { enabled: false; }' \
+  -theme-str 'listview { columns: 3; lines: 3; spacing: 3px; fixed-columns: true; flow: horizontal; }')
 
 if [ -z "$SELECTED_FILENAME" ]; then
-    echo "Selezione annullata."
-    exit 0
+  echo "Selezione annullata."
+  exit 0
 fi
 
 SELECTED_FILE="$WALLPAPER_DIR/$SELECTED_FILENAME"
 
 /usr/bin/swww img "$SELECTED_FILE" \
-    --transition-type "fade" \
-    --transition-duration 3 \
-    --transition-fps 60
+  --transition-type "fade" \
+  --transition-duration 3 \
+  --transition-fps 60
 
-pkill swaync 
-swaync & 
 wal -i "$SELECTED_FILE"
+pkill swaync
+swaync &
 # --- End of Script ---
